046d0f9c65202c8f46c7a724e334122a04f8fa8c
Jeff <110954699+jshriner6@users.noreply.github.com>
2025-08-25 15:59:05 +0000

    Latest build deployed.

--- notes/lunr-pretext-search-index.js
+++ notes/lunr-pretext-search-index.js
@@ -1,76 +1,76 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "sec-I-1",
+  "id": "sec-I-1-sols",
   "level": "1",
-  "url": "sec-I-1.html",
+  "url": "sec-I-1-sols.html",
   "type": "Worksheet",
   "number": "",
   "title": "Introduction and <span class=\"process-math\">\\(\\S I.1\\text{:}\\)<\/span> Sets and Elements",
-  "body": " Introduction and : Sets and Elements    Why? Traditional Calculus vs. Math 156    Motivation: Sets and Elements    Definition: Set   A set is      The following exercises will introduce the important notation (i.e., symbols) used to to communicate sets, and the various ways we can build them.   Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.      The following express TRUE statements. Write out what you think they mean using an English sentence.                            What questions or observations do you have about sets so far?        Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?      Determine if the following are TRUE or FALSE.                    Some Special Sets   The following are commonly used sets that have special notation:                           Practice: True or False?                                        "
+  "body": " Introduction and : Sets and Elements    Why? Traditional Calculus vs. Math 156     Traditional calculus courses typically emphasize algebra\/trigonometry, an introduction to limits (infinity and infinitesimals), and derivatives and integrals (tools for measuring change). Ultimately the goal is to be able to approach differential equations, which help us model physical applications.  In math 156, we emphasize logic\/relations, functions, derivatives and integrals (with applications), and sequences\/series. The goal is to emphasize tools that help us program and compute.     Motivation: Sets and Elements       Sets are one of the most basic structures that we use in mathematics, and they're used often in building other objects and in writing algorithms.     Definition: Set   A set is    a collection of objects, called elements .      The following exercises will introduce the important notation (i.e., symbols) used to to communicate sets, and the various ways we can build them.   Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.    Sets and are the same, and sets and are the same. Two sets are equal if they contain the same elements (order and repetition are disregarded).      The following express TRUE statements. Write out what you think they mean using an English sentence.                             The number is an element of the set .    The number is not an element of the set .    The number of elements (or cardinality ) of sets and is .    The set is an element of the set .         What questions or observations do you have about sets so far?        Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .    The set is all elements in such that is greater than two. The symbol \" \" reads \"such that\". It is also common to use a colon ()\" \") to indicate the same thing. The left hand side of set builder notation indicates a variable for elements in the set, while the right hand side (called the predicate ) indicates a condition that must be met to be in the set.      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?             is the set of all positive even integers.     is also the set of all positive even integers.              Determine if the following are TRUE or FALSE.                   TRUE    FALSE. The left hand side indicate the cardinality of a set, which should be a number, not a set. To make this true, replace with .           Some Special Sets   The following are commonly used sets that have special notation:                              (Integers)     (Natural Numbers)     (Rational Numbers)     (Real Numbers)        Practice: True or False?                                          FALSE    TRUE    FALSE    TRUE    TRUE    FALSE        "
 },
 {
-  "id": "xr-why",
+  "id": "xr-why-sols",
   "level": "2",
-  "url": "sec-I-1.html#xr-why",
+  "url": "sec-I-1-sols.html#xr-why-sols",
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "Why? Traditional Calculus vs. Math 156.",
-  "body": " Why? Traditional Calculus vs. Math 156  "
+  "body": " Why? Traditional Calculus vs. Math 156     Traditional calculus courses typically emphasize algebra\/trigonometry, an introduction to limits (infinity and infinitesimals), and derivatives and integrals (tools for measuring change). Ultimately the goal is to be able to approach differential equations, which help us model physical applications.  In math 156, we emphasize logic\/relations, functions, derivatives and integrals (with applications), and sequences\/series. The goal is to emphasize tools that help us program and compute.   "
 },
 {
-  "id": "xr-motivation",
+  "id": "xr-motivation-sols",
   "level": "2",
-  "url": "sec-I-1.html#xr-motivation",
+  "url": "sec-I-1-sols.html#xr-motivation-sols",
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "Motivation: Sets and Elements.",
-  "body": " Motivation: Sets and Elements  "
+  "body": " Motivation: Sets and Elements       Sets are one of the most basic structures that we use in mathematics, and they're used often in building other objects and in writing algorithms.   "
 },
 {
-  "id": "xr-def-set",
+  "id": "xr-def-set-sols",
   "level": "2",
-  "url": "sec-I-1.html#xr-def-set",
+  "url": "sec-I-1-sols.html#xr-def-set-sols",
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "Definition: Set.",
-  "body": " Definition: Set   A set is   "
+  "body": " Definition: Set   A set is    a collection of objects, called elements .   "
 },
 {
-  "id": "xr-act1",
+  "id": "xr-act1-sols",
   "level": "2",
-  "url": "sec-I-1.html#xr-act1",
+  "url": "sec-I-1-sols.html#xr-act1-sols",
   "type": "Worksheet Exercise",
   "number": "4",
   "title": "Activity: Sets and Elements.",
-  "body": " Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.      The following express TRUE statements. Write out what you think they mean using an English sentence.                            What questions or observations do you have about sets so far?    "
+  "body": " Activity: Sets and Elements   The following are examples of sets. Use them to answer the questions.                                     Do you think any sets are the same? Explain your reasoning.    Sets and are the same, and sets and are the same. Two sets are equal if they contain the same elements (order and repetition are disregarded).      The following express TRUE statements. Write out what you think they mean using an English sentence.                             The number is an element of the set .    The number is not an element of the set .    The number of elements (or cardinality ) of sets and is .    The set is an element of the set .         What questions or observations do you have about sets so far?    "
 },
 {
-  "id": "sec-I-1-4-1",
+  "id": "sec-I-1-sols-4-1",
   "level": "2",
-  "url": "sec-I-1.html#sec-I-1-4-1",
+  "url": "sec-I-1-sols.html#sec-I-1-sols-4-1",
   "type": "Worksheet Exercise",
   "number": "5",
   "title": "Activity: Set Builder Notation.",
-  "body": " Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?      Determine if the following are TRUE or FALSE.                "
+  "body": " Activity: Set Builder Notation   For large or more complicated sets, it's not always convenient (or possible) to write out every element of the set as we did previously. To communicate these sets, we can use set builder notation . The following are examples of sets using set builder notation. Use them to answer the questions.                                It is true that . Write an English sentence that describes set builder notation for .    The set is all elements in such that is greater than two. The symbol \" \" reads \"such that\". It is also common to use a colon ()\" \") to indicate the same thing. The left hand side of set builder notation indicates a variable for elements in the set, while the right hand side (called the predicate ) indicates a condition that must be met to be in the set.      For , write out the sets explicitly, or describe what they are in more common language. Are any sets equal?             is the set of all positive even integers.     is also the set of all positive even integers.              Determine if the following are TRUE or FALSE.                   TRUE    FALSE. The left hand side indicate the cardinality of a set, which should be a number, not a set. To make this true, replace with .       "
 },
 {
-  "id": "sec-I-1-5-1",
+  "id": "sec-I-1-sols-5-1",
   "level": "2",
-  "url": "sec-I-1.html#sec-I-1-5-1",
+  "url": "sec-I-1-sols.html#sec-I-1-sols-5-1",
   "type": "Worksheet Exercise",
   "number": "6",
   "title": "Some Special Sets.",
-  "body": " Some Special Sets   The following are commonly used sets that have special notation:                         "
+  "body": " Some Special Sets   The following are commonly used sets that have special notation:                              (Integers)     (Natural Numbers)     (Rational Numbers)     (Real Numbers)      "
 },
 {
-  "id": "sec-I-1-5-2",
+  "id": "sec-I-1-sols-5-2",
   "level": "2",
-  "url": "sec-I-1.html#sec-I-1-5-2",
+  "url": "sec-I-1-sols.html#sec-I-1-sols-5-2",
   "type": "Worksheet Exercise",
   "number": "7",
   "title": "Practice: True or False?",
-  "body": " Practice: True or False?                                      "
+  "body": " Practice: True or False?                                          FALSE    TRUE    FALSE    TRUE    TRUE    FALSE      "
 },
 {
   "id": "sec-I-2-3-6",
